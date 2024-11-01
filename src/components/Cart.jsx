dc97f3df716da3e4098a07dd368ab77d9942d6d2
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-01 23:54:31 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,7 @@
 import { useContext } from 'react';
 import { CartContext } from './CartContext'; // Make sure the path is correct
 
-const Cart = () => {
-  const { cart, addToCart, removeFromCart } = useContext(CartContext); // Destructure from the context
 
-  if (!cart) {
-    return <div>Cart is empty</div>; // Handle case when cart is undefined
-  }
 
   
    
